ea1313969bca0b94b42e8feb4ac21bc60bf12c59
Scott Dudley <scott@cenotelabs.com>
2025-06-02 15:12:10 -0400

    Use more imported types for Marketplace API requests

--- src/types/marketplace.ts
+++ src/types/marketplace.ts
@@ -1,74 +1,9 @@
 import { components } from './marketplace-api';
 
-export interface Link {
-    /**
-     * Format: uri
-     * @description The link URI - hostname and scheme will be omitted if it is a link within Marketplace
-     */
-    href: string;
-    /** @description Content type of the linked data - may be omitted for JSON resources, will be "text/html" for web pages */
-    type?: string;
-    /** @description Display name of the link - usually omitted */
-    title?: string;
-}
-
-export interface InitiateAsyncLicenseCollectionLinks {
-    self: Link;
-    query: Link;
-    status: Link;
-    download: Link;
-}
-
-export interface InitiateAsyncLicense {
-    /** @description Unique export id for licenses export, for example "12345678-ab12-12ab-1234-123a4b5678ab" */
-    id: string;
-    status: 'IN_PROGRESS' | 'COMPLETED' | 'FAILED';
-    resultUrl?: string;
-    error?: string;
-}
-
-export interface InitiateAsyncLicenseCollection {
-    _links: InitiateAsyncLicenseCollectionLinks;
-    export: InitiateAsyncLicense;
-}
-
-export interface InitiateAsyncTransactionCollectionLinks {
-    self: Link;
-    query: Link;
-    status: Link;
-    download: Link;
-}
-
-export interface InitiateAsyncTransaction {
-    /** @description Unique export id for transactions export */
-    id: string;
-}
-
-export interface InitiateAsyncTransactionCollection {
-    _links: InitiateAsyncTransactionCollectionLinks;
-    export: InitiateAsyncTransaction;
-}
-
-export interface StatusAsyncTransactionCollectionLinks {
-    self: Link;
-    query: Link;
-    download: Link;
-}
-
-export interface StatusAsyncTransaction {
-    /** @description Unique export id for transactions export */
-    id: string;
-    /**
-     * @description Indicates the status of the transaction request
-     * @enum {string}
-     */
-    status: 'QUEUED' | 'IN_PROGRESS' | 'COMPLETED' | 'FAILED';
-}
-
-export interface StatusAsyncTransactionCollection {
-    _links: StatusAsyncTransactionCollectionLinks;
-    export: StatusAsyncTransaction;
-}
+export type InitiateAsyncLicenseCollection = components["schemas"]["InitiateAsyncLicenseCollection"];
+export type InitiateAsyncLicense = components["schemas"]["InitiateAsyncLicense"];
+export type InitiateAsyncTransactionCollection = components["schemas"]["InitiateAsyncTransactionCollection"];
+export type StatusAsyncTransactionCollection = components["schemas"]["StatusAsyncTransactionCollection"];
 
 export type TransactionData = components["schemas"]["Transaction"];
 export type LicenseData = components["schemas"]["License"];
