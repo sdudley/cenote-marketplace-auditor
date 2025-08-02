97ecf9f5001fa738b17b943618a29fbdc02cc4de
Scott Dudley <scott@cenotelabs.com>
2025-08-02 20:46:54 +0200

    Workaround for MPAC /rest/2/addons API deprecation
    Apply the correct user-agent to re-enable the MPAC
    /rest/2/addons API.

--- src/server/services/MarketplaceService.ts
+++ src/server/services/MarketplaceService.ts
@@ -226,7 +226,8 @@ export class MarketplaceService {
 
         const response = await axios.get<components['schemas']['AddonCollection']>(url, {
             headers: {
-                'Authorization': await this.getAuthHeader()
+                'Authorization': await this.getAuthHeader(),
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
             }
         });
 
